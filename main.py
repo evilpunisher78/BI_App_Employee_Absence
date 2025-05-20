fb48cd25a150ab25e74d2db16aa24b30701cf7a2
evilpunisher78 <87294537+evilpunisher78@users.noreply.github.com>
2025-05-20 10:16:41 +0000

    dash run angepasst

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ import webbrowser
 if __name__ == "__main__":
     print("Starte Mitarbeiter-Abwesenheitsmanagement...")
     webbrowser.open("http://127.0.0.1:8050/")
-    app.run_server(debug=True)
+    app.run(debug=True)
